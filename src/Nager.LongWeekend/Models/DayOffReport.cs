934d9447f4a9e854ad02e901ec525477d6cfa0b4
Tino Hager <tino.hager@nager.at>
2024-10-21 21:07:57 +0200

    fix some build warnings

--- src/Nager.LongWeekend/Models/DayOffReport.cs
+++ src/Nager.LongWeekend/Models/DayOffReport.cs
@@ -1,9 +1,23 @@
 ﻿namespace Nager.LongWeekend.Models
 {
+    /// <summary>
+    /// Day Off Report
+    /// </summary>
     public class DayOffReport
     {
+        /// <summary>
+        /// Is a day off possible
+        /// </summary>
         public bool IsDayOffPossible { get; set; }
-        public DateOnly[] BridgeDays { get; set; }
+
+        /// <summary>
+        /// Bridge days
+        /// </summary>
+        public DateOnly[] BridgeDays { get; set; } = [];
+
+        /// <summary>
+        /// Bridge day count
+        /// </summary>
         public int BridgeDayCount {  get; set; }
     }
 }
--- src/Nager.LongWeekend/Models/HolidayRecord.cs
+++ src/Nager.LongWeekend/Models/HolidayRecord.cs
@@ -1,8 +1,18 @@
 ﻿namespace Nager.LongWeekend.Models
 {
+    /// <summary>
+    /// Holiday Record
+    /// </summary>
     public class HolidayRecord
     {
+        /// <summary>
+        /// Date of the holiday
+        /// </summary>
         public DateOnly Date {  get; set; }
+
+        /// <summary>
+        /// Name of the holiday
+        /// </summary>
         public required string Name { get; set; }
     }
 }
